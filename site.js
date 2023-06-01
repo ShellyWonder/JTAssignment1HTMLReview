4216e976b8d5ee496ae26fba80ab1a6743df1909
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-06-01 10:30:56 -0500

    buttons & links
    created js file for btn function. added links and corresponding css

---
+++ site.js
@@ -0,0 +1,15 @@
+function changeNavButtonText() {
+  document.getElementById("navButton").innerHTML = "GitHub Repository";
+}
+
+function changeNavBtnTxtBack() {
+  document.getElementById("navButton").innerHTML = "Try for free";
+}
+
+function changeLeftTopButtonText() {
+  document.getElementById("leftTopButton").innerHTML = "SJ's Portfolio";
+}
+
+function changeLeftTopButtonTxtBack() {
+  document.getElementById("leftTopButton").innerHTML = "Try 5 free sessions";
+}
